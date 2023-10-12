d53f961e819938da58f8ea2bc14dda31c2f37309
anthena0 <apple930615@gmail.com>
2023-10-12 22:04:44 +0800

    1012hw

---
+++ 2.21/2.21/Main.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main() {
+
+	printf("*********\n");
+	printf("*       *\n");
+	printf("*       *\n");
+	printf("*       *\n");
+	printf("*       *\n");
+	printf("*       *\n");
+	printf("*       *\n");
+	printf("*       *\n");
+	printf("*********\n\n");
+
+
+	printf("  ***  \n");
+	printf(" *   *\n");
+	printf("*     *\n");
+	printf("*     *\n");
+	printf("*     *\n");
+	printf("*     *\n");
+	printf("*     *\n");
+	printf("*     *\n");
+	printf("*     *\n");
+	printf(" *   *\n");
+	printf("  ***  \n\n");
+
+
+	printf("   *  \n");
+	printf("  ***  \n");
+	printf(" ***** \n");
+	printf("   *  \n");
+	printf("   *  \n");
+	printf("   *  \n");
+	printf("   *  \n");
+	printf("   *  \n");
+	printf("   *  \n\n");
+
+	printf("    *  \n");
+	printf("   * * \n");
+	printf("  *   *\n");
+	printf(" *     * \n");
+	printf("*       *\n");
+	printf(" *     * \n");
+	printf("  *   *\n");
+	printf("   * * \n");
+	printf("    *  \n");
+	return 0;
+}
